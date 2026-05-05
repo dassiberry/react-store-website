b847fa5ad4d912993ea69fdc51f06644a21894e9
Dassi Berry <dassiberry@gmail.com>
2026-05-05 16:03:22 -0400

    Apply .gitignore and clean up repository

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 
 // Creates root element and renders the App component wrapped in StrictMode
 createRoot(document.getElementById('root')!).render(
